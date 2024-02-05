d00c08952c805092bab6035ab27f9ff54576c891
daviduang <384755564@qq.com>
2024-02-05 15:45:07 +1100

    composer

---
+++ storage/framework/views/036f0cb93fac43b60e5d501ac09a921114bb6c43.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Server Error')); ?>
+<?php $__env->startSection('code', '500'); ?>
+<?php $__env->startSection('message', __('Server Error')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Users/david/Desktop/UTAS/2023 Semester1/KIT721 Web App Devlopment/Assignment3 - IOS/KIT721 Assignment3/KIT721 Assignment3/Assignment2_Laravel/vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/500.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/ab15b657515b29f7c5012e9999d883767df50309.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Users/david/Desktop/UTAS/2023 Semester1/KIT721 Web App Devlopment/Assignment3 - IOS/KIT721 Assignment3/KIT721 Assignment3/Assignment2_Laravel/vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/404.blade.php ENDPATH**/ ?>
